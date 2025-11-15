24c1e39bdfab4a6c4842bd993172774ee81f0eb2
petericranston <39384415+petericranston@users.noreply.github.com>
2025-11-15 01:18:57 +0000

    Got the backend connected and pages working

---
+++ SimpleSocialApp/Server/server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const path = require("path")
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// Index file served
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../Client/index.html"));
+});
+
+app.get("/app", (request, response)=> {
+    response.sendFile(path.join(__dirname, '/views', '/app.html'));
+})
+
+app.get("/login", (request, response)=> {
+    response.sendFile(path.join(__dirname, '/views', '/login.html'));
+})
+
+app.get("/logout", (request, response)=> {
+    response.sendFile(path.join(__dirname, '/views', '/logout.html'));
+})
+
+app.get("/register", (request, response)=> {
+    response.sendFile(path.join(__dirname, '/views', '/register.html'));
+})
+
+const PORT = 3000;
+app.listen(PORT, () => console.log(`Server running on port localhost:${PORT}`));
