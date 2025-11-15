1106919d0b47c761fa556318b2669540567de839
petericranston <39384415+petericranston@users.noreply.github.com>
2025-11-15 02:06:34 +0000

    Attempting to make adding posts work

---
+++ SimpleSocialApp/Server/models/posts.js
@@ -0,0 +1,10 @@
+function addPost(message, user){
+    let newPost={
+        message: message,
+        user: user
+    }
+}
+
+module.exports={
+    addPost
+}
--- SimpleSocialApp/Server/server.js
+++ SimpleSocialApp/Server/server.js
@@ -1,8 +1,10 @@
 const express = require("express");
-const path = require("path")
+const path = require("path");
 const cors = require("cors");
 
 const app = express();
+
+const posts=require('./models/posts.js');
 
 app.use(cors());
 app.use(express.json());
@@ -14,19 +16,24 @@ app.get("/", (req, res) => {
 
 app.get("/app", (request, response)=> {
     response.sendFile(path.join(__dirname, '/views', '/app.html'));
-})
+});
 
 app.get("/login", (request, response)=> {
     response.sendFile(path.join(__dirname, '/views', '/login.html'));
-})
+});
 
 app.get("/logout", (request, response)=> {
     response.sendFile(path.join(__dirname, '/views', '/logout.html'));
-})
+});
 
 app.get("/register", (request, response)=> {
     response.sendFile(path.join(__dirname, '/views', '/register.html'));
-})
+});
+
+app.post('/newpost', (request, response) => {
+    posts.addPost(request.body.message, "UserName");
+    console.log("Message Sent");
+});
 
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Server running on port localhost:${PORT}`));
