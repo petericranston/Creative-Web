a6fa34551fc197a1b1539fef1cec2348bccc7895
petericranston <39384415+petericranston@users.noreply.github.com>
2025-11-24 20:41:47 +0000

    trying to get the front end connected to the backend

--- Creative Web Project/frontend/src/App.jsx
+++ Creative Web Project/frontend/src/App.jsx
@@ -1,9 +1,25 @@
 import React, { useEffect, useState } from "react";
 
 function App() {
-  const [backendData, setBackendData] = useState([{}]);
+  const [backendData, setBackendData] = useState({});
 
-  return <div></div>;
+  useEffect(() => {
+    fetch("/api")
+      .then((response) => response.json())
+      .then((data) => {
+        setBackendData(data);
+      });
+  }, []);
+
+  return (
+    <div>
+      {typeof backendData.users === "undefined" ? (
+        <p>Loading... </p>
+      ) : (
+        backendData.users.map((user, i) => <p key={i}>{user}</p>)
+      )}
+    </div>
+  );
 }
 
 export default App;
