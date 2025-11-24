1cf1136a37768c33e44517983abca71c602aadb1
petericranston <39384415+petericranston@users.noreply.github.com>
2025-11-24 23:28:28 +0000

    Getting started on the server side

--- Creative Web Project/backend/server.js
+++ Creative Web Project/backend/server.js
@@ -1,11 +1,37 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const dotenv = require("dotenv").config(); //Configuring my .env for secret keys (mongodb)
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+const sessions = require("express-session");
+const cookieParser = require("cookie-parser");
+
+const mongoDBUsername = process.env.mongoDBUsername; //Getting data from env file
+const mongoDBPassword = process.env.mongoDBPassword;
+const mongoAppName = process.env.mongoAppName;
+
+const connectionString = `mongodb+srv://${mongoDBUsername}:${mongoDBPassword}@cluster0.hxdji7a.mongodb.net/${mongoAppName}?retryWrites=true&w=majority`;
+//Building connection string for mongodb
+const mongoose = require("mongoose"); //requiring and connecting to mongodb
+mongoose.connect(connectionString);
+
+const threeMinutes = 3 * 60 * 1000; //Variables to decide how long the user will be singed in for
+const oneHour = 1 * 60 * 60 * 1000;
+
+app.use(
+  //Starting a session to keep user signed in and store user data to use throughout the app
+  sessions({
+    secret: "No Secret Yet",
+    cookie: { maxAge: oneHour },
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 
 app.get("/api", (req, res) => {
   res.json({ users: ["one", "two", "three"] });
