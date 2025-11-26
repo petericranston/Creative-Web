84d871d6d91acb4cfd8fc630a795ec1d1d8f31ab
petericranston <39384415+petericranston@users.noreply.github.com>
2025-11-26 02:08:00 +0000

    Trying to get the register page to work

--- Creative Web Project/frontend/src/pages/register.jsx
+++ Creative Web Project/frontend/src/pages/register.jsx
@@ -3,21 +3,41 @@ import { useState } from "react";
 export default function Register() {
   const [formData, setFormData] = useState({ username: "", password: "" });
 
-  async function submit(error) {
-    error.preventDefault();
-    const response = await fetch("api/register", {
+  function handleUsername(e) {
+    setFormData((prev) => ({ ...prev, username: e.target.value }));
+  }
+  function handlePassword(e) {
+    setFormData((prev) => ({ ...prev, password: e.target.value }));
+  }
+
+  async function submit(e) {
+    e.preventDefault();
+    const response = await fetch("/api/register", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(formData),
     });
+    const data = await response.json();
   }
 
   return (
     <div>
       <h1>Register</h1>
       <form onSubmit={submit}>
-        <input type="text" name="username" placeholder="Username" />
-        <input type="password" name="password" placeholder="Password" />
+        <input
+          type="text"
+          name="username"
+          placeholder="Username"
+          value={formData.username}
+          onChange={handleUsername}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          value={formData.password}
+          onChange={handlePassword}
+        />
         <button type="submit">Register</button>
       </form>
     </div>
