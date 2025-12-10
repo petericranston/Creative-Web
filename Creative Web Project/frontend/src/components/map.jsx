0848a76f149d81a5b2684b7a16c21689e5407160
petericranston <39384415+petericranston@users.noreply.github.com>
2025-12-10 22:26:55 +0000

    The map is on the screen but doesn't work well at all yet

--- Creative Web Project/frontend/src/components/map.jsx
+++ Creative Web Project/frontend/src/components/map.jsx
@@ -1,8 +1,28 @@
 import "../styles/map.css";
+import {
+  ImageOverlay,
+  MapContainer,
+  Marker,
+  Popup,
+  useMapEvent,
+} from "react-leaflet";
+import L from "leaflet";
+
 export default function Map() {
+  const width = 1000;
+  const height = 500;
+  const bounds = [
+    [0, 0],
+    [height, width],
+  ];
+
   return (
-    <div>
-      <h2>New Heading</h2>
-    </div>
+    <MapContainer
+      crs={L.CRS.Simple}
+      style={{ height: "100vh", width: "100%" }}
+      bounds={bounds}
+    >
+      <ImageOverlay url="/images/BlankMap.png" bounds={bounds} />
+    </MapContainer>
   );
 }
