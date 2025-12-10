dda4a1d469c49b8e2cea922aa780dcbe68fbc1a7
petericranston <39384415+petericranston@users.noreply.github.com>
2025-12-10 16:44:57 +0000

    Getting map component started

---
+++ Creative Web Project/frontend/src/components/map.jsx
@@ -0,0 +1,8 @@
+import "../styles/map.css";
+export default function Map() {
+  return (
+    <div>
+      <h2>New Heading</h2>
+    </div>
+  );
+}
--- Creative Web Project/frontend/src/pages/create.jsx
+++ Creative Web Project/frontend/src/pages/create.jsx
@@ -1,7 +1,9 @@
+import Map from "../components/map";
 export default function Create() {
   return (
     <div>
       <h1>Create a map</h1>
+      <Map />
     </div>
   );
 }
