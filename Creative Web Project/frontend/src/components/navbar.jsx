80bd84699d6d84551ae8980f60d764ef28ed8c3a
petericranston <39384415+petericranston@users.noreply.github.com>
2025-12-10 05:30:38 +0000

    Adding a stylsheet for nav bar to keep it organised

--- Creative Web Project/frontend/src/components/navbar.jsx
+++ Creative Web Project/frontend/src/components/navbar.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
+import "../styles/navbar.css";
 
 export default function Navbar() {
   const navigate = useNavigate();
