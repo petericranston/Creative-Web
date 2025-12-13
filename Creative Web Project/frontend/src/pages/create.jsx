93d2d0b6a40785cb6518658505226d616feeb358
petericranston <39384415+petericranston@users.noreply.github.com>
2025-12-13 02:29:06 +0000

    Adding new markers locally

--- Creative Web Project/frontend/src/pages/create.jsx
+++ Creative Web Project/frontend/src/pages/create.jsx
@@ -1,23 +1,9 @@
-import { marker } from "leaflet";
+import { marker, popup } from "leaflet";
+import { useState } from "react";
 import Map from "../components/map";
 import "../styles/create.css";
 export default function Create() {
-  async function NewMarker() {
-    try {
-      const response = await fetch("/api/addMarker", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-      });
-      if (!response.ok) {
-        console.log("Marker failed to add");
-        return;
-      }
-    } catch (error) {
-      console.log("Marker failed to add ", error);
-    }
-  }
-
-  const markers = [
+  const [markers, setMarkers] = useState([
     {
       coords: [370, 780],
       popUp: "Kings Landing",
@@ -34,7 +20,25 @@ export default function Create() {
       coords: [450, 600],
       popUp: "Harrenhall",
     },
-  ];
+  ]);
+
+  async function NewMarker() {
+    // try {
+    //   const response = await fetch("/api/addMarker", {
+    //     method: "POST",
+    //     headers: { "Content-Type": "application/json" },
+    //   });
+    //   if (!response.ok) {
+    //     console.log("Marker failed to add");
+    //     return;
+    //   }
+    // } catch (error) {
+    //   console.log("Marker failed to add ", error);
+    // }
+    const newMarker = { coords: [600, 600], popUp: "newMarker" };
+
+    setMarkers((prevMarkers) => [...prevMarkers, newMarker]);
+  }
 
   return (
     <div>
