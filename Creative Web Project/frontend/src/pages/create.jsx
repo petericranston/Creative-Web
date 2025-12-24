7ed1a73d94c0528085e5f52362486a575072bab7
petericranston <39384415+petericranston@users.noreply.github.com>
2025-12-24 00:04:18 +0000

    Removing code i dont need

--- Creative Web Project/frontend/src/pages/create.jsx
+++ Creative Web Project/frontend/src/pages/create.jsx
@@ -51,21 +51,6 @@ export default function Create() {
     const newMarker = { coords: [600, 600], popUp: "newMarker" };
 
     setMarkers((prevMarkers) => [...prevMarkers, newMarker]);
-
-    // try {
-    //   const response = await fetch("/api/addMarker", {
-    //     method: "POST",
-    //     credentials: "include",
-    //     headers: { "Content-Type": "application/json" },
-    //     body: JSON.stringify({ mapID, markers }),
-    //   });
-    //   if (!response.ok) {
-    //     console.log("Marker failed to add.");
-    //     return;
-    //   }
-    // } catch (error) {
-    //   console.log("Marker failed to add ", error);
-    // }
   }
 
   async function saveChanges() {
